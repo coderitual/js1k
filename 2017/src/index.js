2dcc552214adb7aa710a163b66de32288e449218
coderitual <skowronkow@hotmail.com>
2017-02-27 23:00:35 +0100

    Add movement modifier

--- 2017/src/index.js
+++ 2017/src/index.js
@@ -14,17 +14,17 @@ const update = (dt) => {
   c.fillRect(0, 0, w, h);
   c.fillStyle = '#FFF';
 
-  for (let j = 0; j < h / 21; j++) { 
-    for (let i = 0; i < w / 21; i++) {
+  for (let j = 0; j < h / 22; j++) { 
+    for (let i = 0; i < w / 22; i++) {
       const x = i * 22 + (j % 2 ? 0 : 11);
       const y = j * 22;
 
 
-      const v = [m[0] - x, m[1] - y];                   // vector to the middle
-      const vl = Math.sqrt(v[0] * v[0] + v[1] * v[1]);  // vector length
-      const vn = [v[0] / vl, v[1] / vl];                // normalized vector
-
-      drawCircle(x + vn[0] * Math.cos(dt / 1000) * vl, y + vn[1] * Math.cos(dt / 1000) * vl, 2);
+      const v = [m[0] - x, m[1] - y];                         // vector to the middle
+      const vl = Math.sqrt(v[0] * v[0] + v[1] * v[1]);        // vector length
+      const vn = [v[0] / vl, v[1] / vl];                      // normalized vector
+      const mv = (Math.sin(dt / 500 + vl / 200) - 1) * 50;    // movement modifier
+      drawCircle(x + vn[0] * mv, y + vn[1] * mv, 2);
     }
   }
 };
