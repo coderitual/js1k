aef29bac07068045942431bfc83574dc2dd05746
coderitual <skowronkow@hotmail.com>
2017-02-27 23:54:58 +0100

    Add a faster motion blur

--- 2017/src/index.js
+++ 2017/src/index.js
@@ -10,22 +10,23 @@ const drawCircle = (x, y, r) => {
 };
 
 const update = (dt) => {
-  c.fillStyle = '#000';
+  c.fillStyle = "rgba(0, 0, 0, 0.2)";
   c.fillRect(0, 0, w, h);
   c.fillStyle = '#FFF';
-
-  for (let j = 0; j < h / 22; j++) { 
-    for (let i = 0; i < w / 22; i++) {
-      const x = i * 22 + (j % 2 ? 0 : 11);
-      const y = j * 22;
+  const parts = 20;
+  for (let j = 0; j < h / parts; j++) { 
+    for (let i = 0; i < w / parts; i++) {
+      const x = i * parts + (j % 2 ? 0 : parts / 2);
+      const y = j * parts;
 
       const v = [m[0] - x, m[1] - y];                         // vector to the middle
       const vl = Math.sqrt(v[0] * v[0] + v[1] * v[1]);        // vector length
       const vn = [v[0] / vl, v[1] / vl];                      // normalized vector
       const mv = (Math.cos(dt / 360 - vl / 100) - 1) * 50;    // movement modifier
-      drawCircle(x + vn[0] * mv, y + vn[1] * mv, 3);
+      drawCircle(x + vn[0] * mv, y + vn[1] * mv, 2);
     }
   }
+
 };
 
 const loop = (dt) => {
