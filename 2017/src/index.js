cb5a680ff62b5b12c7803596d1f86dc1a74cdbf3
coderitual <skowronkow@hotmail.com>
2017-02-27 23:19:14 +0100

    Tweak params

--- 2017/src/index.js
+++ 2017/src/index.js
@@ -23,8 +23,8 @@ const update = (dt) => {
       const v = [m[0] - x, m[1] - y];                         // vector to the middle
       const vl = Math.sqrt(v[0] * v[0] + v[1] * v[1]);        // vector length
       const vn = [v[0] / vl, v[1] / vl];                      // normalized vector
-      const mv = (Math.cos(dt / 500 - vl / 100) - 1) * 60;    // movement modifier
-      drawCircle(x + vn[0] * mv, y + vn[1] * mv, 2);
+      const mv = (Math.cos(dt / 400 - vl / 100) - 1) * 50;    // movement modifier
+      drawCircle(x + vn[0] * mv, y + vn[1] * mv, 3);
     }
   }
 };
