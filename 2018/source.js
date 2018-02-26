c8175056e6341b598ee7d879751a24e83f76e347
coderitual <skowronkow@gmail.com>
2018-02-26 01:41:49 +0100

    feat(2018): clear template

--- 2018/source.js
+++ 2018/source.js
@@ -1,12 +1,6 @@
-// Template of source code
-// Following code is just a suggestion
+/**
+ * js1k - 2018
+ * coptyright © coderitual
+ */
 
-t = 0;
-
-L = function() {
-  setTimeout(L, 9);
-  c.clearRect(0, 0, a.width, a.height);
-  c.fillRect(0, 10, t++, 50);
-};
-
-L();
+ 
