bd968834467abfbbf8834cfd8070be27c8efe47c
coderitual <skowronkow@gmail.com>
2018-02-28 08:27:15 +0100

    feat(2018): try canvas2d

--- 2018/source.js
+++ 2018/source.js
@@ -9,6 +9,15 @@
  * c = context
  */
 
+b.style.background = 'radial-gradient(circle at 0 0, #555, #222)';
+
+path = new Path2D('M27 19c12.1 0 22.26-1.75 25.17-4.1.77-.3 1.33-1.03 1.33-1.9V6c0-1.1-.9-2-2-2h-49c-1.1 0-2 .9-2 2v7c0 .87.56 1.6 1.33 1.9C4.73 17.24 14.9 19 27 19z');
+
+void function update() {
+  requestAnimationFrame(update);
+  c.fill(path);
+}()
+
 var coin = [
   '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 164 418">',
   '<g class="coin coin--first">',
@@ -18,4 +27,4 @@ var coin = [
   '</svg>'
 ].join('');
 
-a.insertAdjacentHTML('afterend', coin);
+// a.insertAdjacentHTML('afterend', coin);
